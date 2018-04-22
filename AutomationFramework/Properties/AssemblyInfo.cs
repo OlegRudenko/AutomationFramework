54312bb00be4480e8b8ccb4dd2bc2ec4e2869476
Олег Руденко <olegrud14@gmail.com>
2018-04-22 14:32:26 +0300

    Added new Proj

---
+++ AutomationFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutomationFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("91d6cd92-31bf-4752-aea1-337d63bd33d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AutomationFramework/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationFramework
+{
+    [TestFixture]
+    public class Tests
+    {
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://automationpractice.com";
+
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+
+
+
+        }
+        
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+
+
+
+        }
+    }
+}
